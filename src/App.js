7c31d8ee1d1b374d2b3ef529ebb48f6c35d54a37
wang-er <33679181+wang-er@users.noreply.github.com>
2019-03-24 01:01:46 -0400

    wait hmmm

--- src/App.js
+++ src/App.js
@@ -2,18 +2,23 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const client = require('soundoftext-js');
+const Tone = require('tone');
+const tone = require('./N.mp3');
+const noteArray = ["C3", "D3", "E3", "F3", "G3", "A3", "B3", "C4"];
+let samplers = [];
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Potato />
         <header className="App-header">
           <div> Memorize? Harmonize? Melody?</div>
-            <form> <input type="text"></input>
+            <form> <input id="facts" type="text"></input>
                 <br></br>
             </form>
             <br></br>
-            <button>Melodize my knowledge!</button>
+            <Potato />
         </header>
 
       </div>
@@ -22,114 +27,157 @@ class App extends Component {
 }
 
 class Potato extends Component {
+  constructor(props) {
+      super(props);
+      this.state = { sampler: null, samplers : null };
+
+      this.run = this.run.bind(this);
+  }
+  componentDidMount() {
+      var sampler = new Tone.Sampler({
+          "C3" : "https://soundoftext.nyc3.digitaloceanspaces.com/a5bdc090-f922-11e7-b289-2f4fa9c8406d.mp3"
+      }).toMaster();
+      this.setState({sampler});
+      // sampler.triggerAttackRelease("D3", "8n");
+  }
+
   render() {
     return (
-        <div> HOW WONDERFUL
-            <button id="Oof" onClick={this.run}> Off ouch my npn bones </button></div>
+        <div>
+            <button id="Oof" onClick={this.run}> Memorize button sound! </button></div>
 
     )
   }
 
+
   run() {
+      let nameThing ="";
+      let _this = this;
+      let textContent = document.getElementById("facts").value;
+      let words = textContent.split(" ");
+      let promises = [];
 
-      const client = require('soundoftext-js');
-      const Tone = require('tone');
-      var nameThing ="";
+      for(let i = words.length -1 ; i >= 0; i--) {
+
+          promises.push(client.sounds.create({ text:words[i], voice: 'en-US' }))
+
+      }
+
+      Promise.all(promises).then((results) => {
+          console.log(results);
+
+          let input = this.makeNoteObjects(results);
+
+          this.setSampler(input);
 
 
-      client.sounds.create({ text:'Hello, world!', voice: 'en-US' })
-          .then(soundUrl => {
-              console.log(soundUrl); // https://soundoftext.nyc3.digitaloceanspaces.com/<sound-id>.mp3
-              nameThing = soundUrl;
-
-              console.log(nameThing + " e hhh");
-
-
-          })
-          .catch(e => {
-              /* Reasons that the Promise might get rejected:
-               * - after 60 seconds, it automatically times out
-               * - the API might fail to create the sound or reject it
-               * - other miscellaneous network issues
-               */
-          });
-
+      });
 
 
 
+
+      //hmmm pause?????
+
+
+
+      // let values = {};
       //
-      // var sampler = new Tone.Sampler({
-      //     "C3" : nameThing,
-      //     "D3" : nameThing,
-      //     // "D#3" : "path/to/Dsharp3.mp3",
-      //     // "F#3" : "path/to/Fsharp3.mp3",
-      //     // "A3" : "path/to/A3.mp3",
+      // for(let i = 0; i < mp3links; i++) {
+      //     let note= Math.floor((Math.random() * noteArray.length));
+      //     values[note] = mp3links[i];
+      //     values.i = "hi";
+      // }
+
+      // console.log(values);
+
+
+      // var pianoSamples = new Tone.Buffers({
+      //     "C3" : "https://tonejs.github.io/examples/audio/casio/C2.mp3"
       // }, function(){
-      //     //sampler will repitch the closest sample
-      //     sampler.triggerAttack("D3")
-      // }).toMaster();
-      //
-      // sampler.triggerAttackRelease("D3", "8n")
-
-
-      // var sampler = new Tone.Sampler({
-      //     'C3' : nameThing
-      //     //'D3' : 'tones.[mp3|ogg]',
-      //     //'E3' : 'cymbal.[mp3|ogg]',
-      //     //'F3' : 'piano.[mp3|ogg]',
-      //     //'G3' : 'synth.[mp3|ogg]',
-      // }, {
-      //     'release' : 1,
-      // }).toMaster();
-      // sampler.triggerAttack("C3")
-
-
-      // var sampler = new Tone.Sampler({
-      //     "A0" :  nameThing,
-      // }, {
-      //     "release" : 1,
-      //     "baseUrl" : "./audio/salamander/"
-      // }).toMaster();
-
-//
-//       var player = new Tone.Player(nameThing).toMaster();
-// //play as soon as the buffer is loaded
-//       player.autostart = true;
-//
-//       player.triggerAttackRelease("A4", "8n")
-
-
-
-      // sampler.triggerAttackRelease("A4", "8n");
-
-
-      // sampler.triggerAttackRelease("A4", "8n");
-
-      // var buffer = new Tone.Buffer("./public/N.mp3", function(){
-      //     //the buffer is now available.
-      //     var buff = buffer.get();
+      //     //play one of the samples when they all load
+      //     pianoSamples.get("C3").toMaster().triggerAttackRelease("C3", "8n");
       // });
 
 
-//
-// //create a synth and connect it to the master output (your speakers)
-//       var synth = new Tone.Synth().toMaster();
+      // for(let i = 0; i < words.length; i++) {
+      //     samplers.push(new Tone.Sampler(
+      //         {
+      //         "C3" : mp3links[i],
+      //     }
+      //     ));
+      //     // sampler.triggerAttackRelease("D3", "8n");
+      // }
+
 
       //
-      // var synth = new Tone.Sampler({'C3' : buffer
-      // }).toMaster();
+      // console.log(samplers);
       //
-      // synth.buffer = buffer;
       //
-      // console.log(synth.loaded);
+      // samplers[0].toMaster().triggerAttackRelease("C3", "8n");
+      //this.setState({samplers});
 
-      var player = new Tone.Player("./public/N.mp3").toMaster();
-//play as soon as the buffer is loaded
-      player.autostart = true;
+      //
+      //
+      // for(let i = 0; i < samplers.length - 1; i++){
+      //     let rand = Math.floor((Math.random() * noteArray.length));
+      //     samplers[0].triggerAttackRelease("C3", "8n");
+      //     // this.state.sampler.triggerAttackRelease(noteArray[rand], "1n");
+      // }
 
-// //play a middle 'C' for the duration of an 8th note
-//       synth.triggerAttack('C3', "1n");
+
+
+
+        // this.state.sampler.triggerAttackRelease("E3", "1n");
+
+
+
   }
+
+  makeNoteObjects(results) {
+      let values = {};
+
+      for(let i = 0; i < results.length; i++) {
+          let note = Math.floor((Math.random() * noteArray.length));
+          values[noteArray[note]] = results[i];
+      }
+
+      console.log(values);
+
+      return values;
+
+  }
+
+
+  setSampler(input) {
+      console.log("reached setSampler");
+      let sampler = new Tone.Sampler(
+              input , function() {
+                  console.log("reached function inside sampler")
+                  console.log(Object.keys(input).length + " hellppppp");
+
+                  for(let i = 0; i < Object.keys(input).length; i++) {
+                      sampler.triggerAttackRelease(Object.keys(input), "1n");
+                      // this.sleep(10000);
+                      // new Tone.Delay(10, 0);
+                  }
+
+            }
+          ).toMaster();
+          // sampler.triggerAttackRelease("D3", "8n");
+
+  }
+
+  // sleep(ms) {
+  //     var currentTime = new Date().getTime();
+  //
+  //     while(currentTime + ms >= new Date().getTime()){
+  //
+  //     }
+  // }
+
+
+
+
 }
 
 export default App;
